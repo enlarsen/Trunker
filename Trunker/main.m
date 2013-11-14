4e32e321527df3589cfc9455f70333ff7c0e2b3a
Erik Larsen <erik_larsen@icloud.com>
2013-11-14 09:50:36 -0800

    Initial Commit

---
+++ Trunker/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Trunker
+//
+//  Created by Erik Larsen on 11/14/13.
+//  Copyright (c) 2013 Erik Larsen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
